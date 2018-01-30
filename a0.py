5945605b342e8b41772e4715c10dcb6c69ca9e21
Darshan Doshi <33645939+darshandoshi95@users.noreply.github.com>
2018-01-29 18:41:26 -0600

    Update a0.py

--- a0.py
+++ a0.py
@@ -1,33 +1,5 @@
 # coding: utf-8
 
-"""
-CS579: Assignment 0
-Collecting a political social network
-
-In this assignment, I've given you a list of Twitter accounts of 4
-U.S. presedential candidates from the previous election.
-
-The goal is to use the Twitter API to construct a social network of these
-accounts. We will then use the [networkx](http://networkx.github.io/) library
-to plot these links, as well as print some statistics of the resulting graph.
-
-1. Create an account on [twitter.com](http://twitter.com).
-2. Generate authentication tokens by following the instructions [here](https://developer.twitter.com/en/docs/basics/authentication/guides/access-tokens.html).
-3. Add your tokens to the key/token variables below. (API Key == Consumer Key)
-4. Be sure you've installed the Python modules
-[networkx](http://networkx.github.io/) and
-[TwitterAPI](https://github.com/geduldig/TwitterAPI). Assuming you've already
-installed [pip](http://pip.readthedocs.org/en/latest/installing.html), you can
-do this with `pip install networkx TwitterAPI`.
-
-OK, now you're ready to start collecting some data!
-
-I've provided a partial implementation below. Your job is to complete the
-code where indicated.  You need to modify the 10 methods indicated by
-#TODO.
-
-Your output should match the sample provided in Log.txt.
-"""
 
 # Imports you'll need.
 from collections import Counter
